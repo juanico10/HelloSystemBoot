7aba379262126482f55e6dee999b56861b802d44
Juan Rodenas <95724863+JuanRodenas@users.noreply.github.com>
2022-02-11 03:55:44 +0100

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+# requeriments:
 TOKEN="YOUR_TOKEN_BOT"
 ID="YOUR_TOKEN_CHAT"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 DNS="1.1.1.1"
-Servidor=$(hostname)
-LOCAL_IP=$(ip route get $DNS | grep src| sed 's/.*src \(.*\)$/\1/g' | awk '{print $1}')
+servidor=$(hostname)
+MSG="\U1F50B $servidor, online! \U1F6A8"
+IP=`hostname -I | awk '{print $1}'`
 PUBLIC_IP=$(curl -s -X GET http://checkip.amazonaws.com)
 cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
 FREERAM=$(free -mh | grep Mem | awk '{print $4}')
@@ -21,7 +22,7 @@ else
         curl -s -X POST $URL \
         -d chat_id=$ID \
         -d parse_mode=HTML \
-        -d text="$(printf "$MSG $(date +"%d-%m-%Y %H:%M:%S")<code>\n\t\t- SERVER: $servidor\n\t\t- CPU temp: $((cpu/1000))°\n\t\t- RAM: Free $FREERAM/ Total $TOTALRAM\n\t\t- IP address: $LOCAL_IP\n\t\t- IP public: $PUBLIC_IP</code>")" \
+        -d text="$(printf "$MSG<code>\n\t\t- Host: $servidor\n\t\t- CPU temp: $((cpu/1000))°\n\t\t- RAM: Free $FREERAM/ Total $TOTALRAM\n\t\t- IP: $IP\n\t\t- IP public: $PUBLIC_IP</code>")" \
                 > /dev/null 2>&1
         exit 0
 fi
